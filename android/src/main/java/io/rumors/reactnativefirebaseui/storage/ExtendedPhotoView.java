57c5f12d30f382c5baee2d8f4447a02594714bba
Omer Levy <omer@rumors.io>
2017-10-13 17:56:52 +0300

    update dependencies versions

--- android/src/main/java/io/rumors/reactnativefirebaseui/storage/ExtendedPhotoView.java
+++ android/src/main/java/io/rumors/reactnativefirebaseui/storage/ExtendedPhotoView.java
@@ -16,7 +16,9 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.Transformation;
 import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
-
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.module.AppGlideModule;
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation.CornerType;
@@ -49,27 +51,28 @@ public class ExtendedPhotoView extends PhotoView {
 
   public void updateView() {
     StorageReference storageReference = FirebaseStorage.getInstance().getReference(mPath);
-    FirebaseImageLoader imageLoader = new FirebaseImageLoader();
 
     ArrayList<Transformation> transformations = new ArrayList<Transformation>(1 + mBorderRadii.size());
 
     if (mScaleType == ScaleType.CENTER_CROP) {
-      transformations.add(new CenterCrop(mContext));
+      transformations.add(new CenterCrop());
     } else {
-      transformations.add(new FitCenter(mContext));
+      transformations.add(new FitCenter());
     }
 
     for (Entry<CornerType, Integer> entry : mBorderRadii.entrySet()) {
       CornerType cornerType = entry.getKey();
       Integer radius = entry.getValue();
-      transformations.add(new RoundedCornersTransformation(mContext, radius, 0, cornerType));
+      transformations.add(new RoundedCornersTransformation(radius, 0, cornerType));
     }
 
-    Transformation[] transformationsArray = transformations.toArray(new Transformation[transformations.size()]);
 
-    Glide.with(mContext).using(imageLoader)
+    Transformation[] transformationsArray = transformations.toArray(new Transformation[transformations.size()]);
+    MultiTransformation multi = new MultiTransformation<>(transformationsArray);
+
+    GlideApp.with(mContext)
             .load(storageReference)
-            .bitmapTransform(transformationsArray)
+            .apply(bitmapTransform(multi))
             .into(this);
   }
 }
