82ac2d4b00ce0b0a84a24296d51b5859d3b877a8
erezrokah <erezrokah@users.noreply.github.com>
2018-11-08 16:58:10 +0200

    add firebaseui to example

--- example/App.js
+++ example/App.js
@@ -1,65 +1,47 @@
-import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native'
+import { ImageView, PhotoView } from 'react-native-firebaseui'
 
-import firebase from 'react-native-firebase';
+//no zoom support
+class MyFirebaseImageView extends Component {
+  render() {
+    let imageProps = this.props
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+    return (
+      <ImageView
+        {...imageProps}
+        path='images/firebase_logo.png'
+        timestamp={1000} //optional, can be used to specify last modified time for same storage path
+        resizeMode='cover' //'cover', 'contain', 'stretch'
+      />
+    )
   }
+}
 
-  async componentDidMount() {
-    // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
-    // console.warn('User -> ', user.toJSON());
+//zoom support (android only). On iOS just wrap the ImageView with a scroll view
+class MyFirebasePhotoView extends Component {
+  render() {
+    let imageProps = this.props
 
-    // await firebase.analytics().logEvent('foo', { bar: '123'});
+    return (
+      <PhotoView
+        {...imageProps}
+        path='images/firebase_logo.png'
+        timestamp={1000} //optional, can be used to specify last modified time for same storage path
+        resizeMode='cover' //'cover', 'contain', 'stretch'
+      />
+    )
   }
+}
 
+export default class App extends Component {
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
-          <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          {Platform.OS === 'ios' ? (
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
-        </View>
-      </ScrollView>
-    );
+      <View style={styles.container}>        
+        <MyFirebaseImageView />
+        <MyFirebasePhotoView />
+      </View>
+    )
   }
 }
 
@@ -69,34 +51,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  logo: {
-    height: 120,
-    marginBottom: 16,
-    marginTop: 64,
-    padding: 10,
-    width: 135,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  }
+  },  
 });
--- example/android/app/src/main/java/com/rmrs/reactnativefirebaseuiexample/MainApplication.java
+++ example/android/app/src/main/java/com/rmrs/reactnativefirebaseuiexample/MainApplication.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.rumors.reactnativefirebaseui.RNFirebaseUiPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
+        new RNFirebaseUiPackage(),
         new RNFirebasePackage(),
         // add/remove these packages as appropriate
         new RNFirebaseAdMobPackage(),
