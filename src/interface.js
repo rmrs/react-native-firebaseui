0f076f2dd68d26e90954f596ab58364aef6c658f
Hein Rutjes <hrutjes@gmail.com>
2019-03-14 14:08:21 +0100

    Updated JavaScript code to support the defaultSource prop

---
+++ src/interface.js
@@ -0,0 +1,13 @@
+import { Image } from 'react-native'
+import PropTypes from 'prop-types'
+
+module.exports = {
+  name: 'FirebaseImage',
+  propTypes: {
+    ...Image.propTypes,
+    path: PropTypes.string,
+    timestamp: PropTypes.number,
+    resizeMode: PropTypes.oneOf(['cover', 'contain', 'stretch']),
+    defaultSource: PropTypes.number
+  }
+}
